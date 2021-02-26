8e2ac8629248504b5cb8c76111941d53c497f9dc
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-26 11:33:31 -0300

    create method getList in categories

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -3,12 +3,35 @@ $(document).ready(function () {
 });
 
 function getList() {
+  var tr = $(".table tbody tr");
+  tr.remove();
+
   $.ajax({
     url: "http://localhost:3000/category/list",
     type: "GET",
     async: true,
     success: function (categoryList) {
       console.log(categoryList);
+
+      var table = $(".table tbody");
+      var dataLength = categoryList.length;
+      for (var i = 0; i < dataLength; i++) {
+        var id = categoryList[i].id_categoria;
+        var name = categoryList[i].categoria;
+        var icon = categoryList[i].url_icon;
+
+        table.append(
+          `<tr>
+              <th scope="row">${id}</th>
+              <td>${name}</td>
+              <td>
+                <span class="badge badge-secondary material-icons mr-2">
+                  ${icon}
+                </span>
+              </td>
+          </tr>`
+        );
+      }
     },
   });
 }
