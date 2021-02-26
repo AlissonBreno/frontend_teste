fb3e6011ce8a0295ac5a5bea7543a3e24081950f
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-26 13:01:24 -0300

    create method update in categories

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -21,17 +21,56 @@ function getList() {
         var icon = categoryList[i].url_icon;
 
         table.append(
-          `<tr>
-              <th scope="row">${id}</th>
+          `<tr id="row_${id}">
+              <td scope="row">${id}</td>
               <td>${name}</td>
               <td>
                 <span class="badge badge-secondary material-icons mr-2">
                   ${icon}
                 </span>
               </td>
+              <td>
+                <a href="#" onclick="changeCard('list', 'cadastro', 'category', ${id})" class="btn btn-light btn-sm"><span class="material-icons" aria-hidden="true">mode_edit</span></a>
+                <a href="#" onclick="changeCard('list', 'cadastro', 'category', ${id})" class="btn btn-light btn-sm"><span class="material-icons" aria-hidden="true">delete</span></a>
+              </td>
           </tr>`
         );
       }
+    },
+  });
+}
+
+function save(input) {
+  var id = $(`#${input}`).val();
+
+  if (id == "") {
+    create();
+  } else {
+    update(id);
+  }
+}
+
+function update(id) {
+  var nome = $("#inputNome").val();
+  var icone = $(".active input").val();
+
+  if (nome == "") {
+    return;
+  }
+
+  var body = {
+    categoria: nome,
+    url_icon: icone,
+  };
+
+  $.ajax({
+    url: `http://localhost:3000/category/${id}`,
+    type: "PATCH",
+    data: body,
+    async: true,
+    success: function (category) {
+      console.log(category);
+      changeCard("cadastro", "list", "category");
     },
   });
 }
@@ -56,6 +95,17 @@ function create() {
     async: true,
     success: function (category) {
       console.log(category);
+      changeCard("cadastro", "list", "category");
     },
   });
+}
+
+function loadData(id) {
+  if (id != null) {
+    var list = document.getElementById(`row_${id}`);
+    var elementos = list.getElementsByTagName("td");
+
+    $("#id_categoria").val(id);
+    $("#inputNome").val(elementos[1].innerHTML);
+  }
 }
--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,11 +2,16 @@ $(document).ready(function () {
   $("#cadastro").hide();
 });
 
-function changeCard(fromId, toId) {
+function changeCard(fromId, toId, module, id = null) {
   if (toId.includes("list")) {
     getList();
   }
 
   $(`#${fromId}`).hide();
   $(`#${toId}`).show();
+
+  if (module == "category") {
+    $("#inputNome").val("");
+    loadData(id);
+  }
 }
